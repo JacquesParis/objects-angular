7025889fcffba718e4eb76b40ca3f3ad7eb482cf
Jacques Lebourgeois <jacques@ochoeurdunet.org>
2021-01-03 19:03:58 +0100

    remove aot option

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import { initApp } from './app/app.init';
-import '@angular/compiler';
 
 if (environment.production) {
   enableProdMode();
