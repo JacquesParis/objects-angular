c0717992ad706b79ee7530d5dd85f5aac90d44e0
Jacques Lebourgeois <jacques@ochoeurdunet.org>
2021-01-03 01:10:40 +0100

    add compiler

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import { initApp } from './app/app.init';
+import '@angular/compiler';
+
 if (environment.production) {
   enableProdMode();
 }
